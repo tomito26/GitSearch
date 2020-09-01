0f44841f112532fa5bd6c1218aceabc7dbd3f704
tomito26 <tommybwah@gmail.com>
2020-09-01 10:18:18 +0300

    added fixed property to navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl :'https://api.github.com/users/',
+  gitToken: '?access_token=f8e9ab0a75524787668f7e95c0f97342826200e4' 
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl :'https://api.github.com/users/',
   gitToken: '?access_token=f8e9ab0a75524787668f7e95c0f97342826200e4' 
 
